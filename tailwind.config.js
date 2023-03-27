43624214f30948591cfa1f3dd4a26fc01c32ec57
Pham Hieu <hieuxinhe94@gmail.com>
2023-03-28 00:26:53 +0700

    try to bind tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{html,js}",
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
+    './public/index.html',
+    './Components/**/*.js',
+    './pages/**/*.js',
   ],
   theme: {
     extend: {},
