93d5e4b3a9157de708376936a38edf31af6d4cd7
Pham Hieu <hieuxinhe94@gmail.com>
2023-05-23 22:23:16 +0700

    update view

--- pages/showcases.js
+++ pages/showcases.js
@@ -48,64 +48,72 @@ export default function ShowCases(props) {
                 <h2 className="text-base font-medium default:text-[#08445E] uppercase mb-7 richtext">
                   {showcasesSectionConfig?.title}
                 </h2>
-                <p className="text-xl font-semibold h-36 mr-32 text-gray-300">
-                  <TypeIt
-                    options={{
-                      strings: [
-                 
-                        showcasesSectionConfig?.description,
-                      ],
-                      speed: 0.5,
-                      startDelay: 1000,
-                      waitUntilVisible: true,
-                    }}
-                  ></TypeIt>
-                </p>
+
+                <div
+                  className="relative"
+                  style={{ backgroundImage: "team.svg" }}
+                >
+                  <p className="text-lg h-32 mr-32 text-gray-300">
+                    <TypeIt
+                      options={{
+                        strings: [showcasesSectionConfig?.description],
+                        speed: 0.5,
+                        startDelay: 1000,
+                        waitUntilVisible: true,
+                      }}
+                    ></TypeIt>
+                  </p>
+                  <div className="lg:w-2/3">
+                    {posts
+                      ? posts.map((post, key) => {
+                          return (
+                            <a
+                              href={"#" + key}
+                              className="text-sm overflow-hidden hover:bg-gray-200/20 border-b pb-4 font-medium default:flex default:border-black w-4/5"
+                            >
+                              <svg
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                                focusable="false"
+                                className="reduced-motion:group-hover:-translate-x-full h-6 w-6 origin-left -translate-x-full self-center opacity-0 transition-all duration-500 will-change-transform group-hover:translate-x-0 group-hover:opacity-100"
+                              >
+                                <path
+                                  d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
+                                  fill="currentColor"
+                                ></path>
+                              </svg>
+                              <span className="text-sm reduced-motion:group-hover:translate-x-0 -translate-x-5 transition-transform duration-500 will-change-transform group-hover:translate-x-2">
+                                {post.title}
+                              </span>
+                              <svg
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                                focusable="false"
+                                className="reduced-motion:group-hover:translate-x-0 ml-auto h-6 w-6 origin-left translate-x-0 self-center justify-self-end opacity-100 transition-all duration-500 will-change-transform group-hover:translate-x-full group-hover:opacity-0"
+                              >
+                                <path
+                                  d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
+                                  fill="currentColor"
+                                ></path>
+                              </svg>
+                            </a>
+                          );
+                        })
+                      : null}
+                  </div>
+                </div>
               </div>
               <div data-aos="fade-right" className="md:col-span-4 col-span-12">
                 <p className="tracking-ff-tighter text-shade-70  richtext"></p>
-
-                {posts
-                  ? posts.map((post, key) => {
-                      return (
-                        <a
-                          href={"#" + key}
-                          className="text-sm overflow-hidden hover:bg-gray-200/20 border-b pb-4 font-medium default:flex default:border-black w-4/5"
-                        >
-                          <svg
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            focusable="false"
-                            className="reduced-motion:group-hover:-translate-x-full h-6 w-6 origin-left -translate-x-full self-center opacity-0 transition-all duration-500 will-change-transform group-hover:translate-x-0 group-hover:opacity-100"
-                          >
-                            <path
-                              d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
-                              fill="currentColor"
-                            ></path>
-                          </svg>
-                          <span
-                         
-                            className="text-sm reduced-motion:group-hover:translate-x-0 -translate-x-5 transition-transform duration-500 will-change-transform group-hover:translate-x-2"
-                          >
-                            {post.title}
-                          </span>
-                          <svg
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                            focusable="false"
-                            className="reduced-motion:group-hover:translate-x-0 ml-auto h-6 w-6 origin-left translate-x-0 self-center justify-self-end opacity-100 transition-all duration-500 will-change-transform group-hover:translate-x-full group-hover:opacity-0"
-                          >
-                            <path
-                              d="M17.707 9.293l-5-5a.999.999 0 10-1.414 1.414L14.586 9H3a1 1 0 100 2h11.586l-3.293 3.293a.999.999 0 101.414 1.414l5-5a.999.999 0 000-1.414z"
-                              fill="currentColor"
-                            ></path>
-                          </svg>
-                        </a>
-                      );
-                    })
-                  : null}
+                <img
+                  className="w-full h-128 items-right align-right md:w-1/3-gutter aspect-square snap-center first:ml-gutter last:mr-gutter md:first:ml-0 md:last:mr-0"
+                  sizes="10vw"
+                  src={"team.svg"}
+                  loading="lazy"
+                  alt="Marketing product examples"
+                />
               </div>
             </div>
 
