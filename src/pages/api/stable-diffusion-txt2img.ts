d645658186245c7ef16919b02f7010c4496b44ca
theLemonday <nhathao090703@gmail.com>
2024-06-02 23:31:10 +0700

    feat(integrate-image-gen): create a api for generating image, implement simple ui for demostration
    create public api /api/stable-diffusion-img2img
    implement simple ui for demostration, which include:
    - prompt
    - negative prompt
    - image
    style will be use in the future

---
+++ src/pages/api/stable-diffusion-txt2img.ts
@@ -0,0 +1,68 @@
+import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
+import z from 'zod'
+
+const requestSchema = z.object({
+  prompt: z.string().min(1),
+  negative_prompt: z.string(),
+  image: z.string(),
+})
+
+const STABLE_DIFFUSION_SERVER_URL = 'https://e8bc-14-231-197-33.ngrok-free.app'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Only POST requests are allowed' })
+  }
+
+  const validatedSchema = requestSchema.safeParse(req.body)
+
+  if (!validatedSchema.success) {
+    return res.status(400).json({ message: 'Invalid request' })
+  }
+
+  try {
+    const response = await axios.post(
+      `${STABLE_DIFFUSION_SERVER_URL}/sdapi/v1/img2img`,
+      {
+        prompt: validatedSchema.data.prompt,
+        negative_prompt: validatedSchema.data.negative_prompt,
+        scheduler: 'Karras',
+        seed: -1,
+        steps: 20,
+        width: 512,
+        height: 512,
+        denoising_strength: 0.5,
+        n_iter: 1,
+        init_images: [validatedSchema.data.image],
+        alwayson_scripts: {
+          controlnet: {
+            args: [
+              {
+                module: 'canny',
+                model: 'control_canny-fp16 [e3fe7712]',
+              },
+            ],
+          },
+        },
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+
+    const data = response.data
+
+    res.status(200).json({ result: data.images[0] })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: 'Error generating response', error: error.message })
+  }
+}
